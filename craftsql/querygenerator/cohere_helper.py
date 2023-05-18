6d97e7511832e9b7e340cd7919e906d8e908e7e1
Navneet Desai <navneetdesai44@gmail.com>
2023-05-18 14:12:17 +0530

    Use cohere instead of airops to avoid api issues

--- craftsql/querygenerator/cohere_helper.py
+++ craftsql/querygenerator/cohere_helper.py
@@ -7,3 +7,12 @@ co = cohere.Client(auth.COHERE_API)
 def generate_query(query_input):
     query_input = f'Write an SQL query for this task: {query_input}'
     return co.generate(model='command', prompt=f'{query_input}\n', max_tokens=2000, temperature=0, k=0, stop_sequences=[], return_likelihoods='NONE')
+
+
+def explain_query(query):
+    query_input = f'Explain this query in detail, be as accurate as possible: {query}'
+    return co.generate(model='command', prompt=f'{query_input}\n', max_tokens=2000, temperature=0, k=0, stop_sequences=[], return_likelihoods='NONE')
+
+def fix_query(query):
+    query_input = f'Fix this query. Suggest results for Postgres, MySQL: {query}'
+    return co.generate(model='command', prompt=f'{query_input}\n', max_tokens=2000, temperature=0, k=0, stop_sequences=[], return_likelihoods='NONE')
--- craftsql/querygenerator/views.py
+++ craftsql/querygenerator/views.py
@@ -29,56 +29,28 @@ def generate(request):
 def explain_page(request):
     return render(request, 'explainpage.html')
 
-
-def explain_sql(request):
-    if request.method != 'POST':
-        return render(request, 'explain.html')
-    query = request.POST.get('query', '')
-
-    api_url = 'https://api.airops.com/explain-query'
-    headers = {'Authorization': f'Bearer {AIROPS_API}'}
-    data = {'input': {'query': query}}
-    print(headers)
-    response = requests.post(api_url, headers=headers, json=data)
-
-    print(response.text)
-    if response.status_code == 200:
-        explanation = response.json().get('explanation')
-    else:
-        explanation = 'Failed to get explanation. Please try again.'
-
-    return render(request, 'explainpage.html', {'explanation': explanation})
-
-
 def fixpage(request):
     return render(request, 'fixpage.html')
 
 def suggest(request):
     return render(request, 'suggest.html')
 
+def explain_sql(request):
+    if request.method != 'POST':
+        return render(request, 'explain.html')
+    query = request.POST.get('query', '')
+    if not query:
+        return render(request, 'explain.html')
+    response = cohere_helper.explain_query(query)
+    return render(request, 'explainpage.html', {'explanation': response[0].text})
+
 
 def fix_query(request):
     if request.method == 'POST':
-        query = request.POST.get('query', '')
-
-        api_url = 'https://api.airops.ai/fix-query'
-        headers = {
-            'Authorization': f'Bearer {AIROPS_API}',
-            'Content-Type': 'application/json'
-        }
-        data = {
-            'query': query
-        }
-        response = requests.post(api_url, headers=headers, json=data)
-
-        if response.status_code == 200:
-            fixed_query = response.json().get('fixed_query', '')
-            return render(request, 'fix_results.html', {'fixed_query': fixed_query})
-        else:
-            error_message = 'Error occurred while fixing the query.'
-            return render(request, 'fix_results.html', {'error_message': error_message})
-
-    return render(request, 'fix_query.html')
+        if query := request.POST.get('query', ''):
+            response = cohere_helper.fix_query(query)
+            return render(request, 'fixpage.html', {'results': response[0].text[:]})
+    return render(request, 'fixpage.html')
 
 
 def suggest_optimization(request):
